ee98851fcc7ea70c53b206de5d2a88799b0e2f3e
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 00:04:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,10 @@
 def bash_hash
   { railroads: {} }
 end
+
+def monopoly_with_second_tier
+  hash = bash_hash
+  hash[:railroads][:pieces] = 4
+  hash[:railroads][:rent_in_dollars] = {}
+  hash[:railroads][:names] = {}
+end
