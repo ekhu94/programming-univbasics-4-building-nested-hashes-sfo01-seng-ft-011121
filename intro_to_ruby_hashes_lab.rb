89efa6c66d2d7b86b1e968eacebbad0864e9052b
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 00:12:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ end
 
 def monopoly_with_second_tier
   hash = bash_hash
-  hash[:railroads][:pieces] = 4
+  hash[:railroads][:pieces] = "4"
   hash[:railroads][:rent_in_dollars] = {}
   hash[:railroads][:names] = {}
   hash
